91c32f8e7980535698908b67237353413ccb9e97
Ganesh Acharya <ganesh.a@juegostudio.com>
2019-07-16 10:26:41 +0530

    Learning map reduce filter

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-const timeFinder = require('./logger')
+// const timeFinder = require('./logger')
+
+let data = [
+    10, 12, 13, 14
+];
+
+const indexes = data.map((dataitem) => dataitem);
+
+
+data.pop()
+
+
+console.log("Source: " + data)
+console.log("Target: " + indexes)
